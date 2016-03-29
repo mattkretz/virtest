c961b25cdb4b297e361b4f946ca16f15dc0c9f79
Matthias Kretz <kretz@kde.org>
2016-03-29 17:00:55 +0200

    Vc 2.x related cleanups
    - The datapar test checks conversions from any to any and correctly
    skips compares that compare results from conversions with undefined
    behavior.
    - unittest.h doesn't depend on <Vc/Vc> anymore; it includes <Vc/datapar>
    instead. This required removal of many explict Vc::Vector mentions.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- vir/typetostring.h
+++ vir/typetostring.h
@@ -1,4 +1,4 @@
-/*  This file is part of the Vc library. {{{
+/*{{{
 Copyright © 2014-2015 Matthias Kretz <kretz@kde.org>
 
 Redistribution and use in source and binary forms, with or without
@@ -28,8 +28,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VC_TO_STRING_H
 #include <sstream>
 #include "typelist.h"
-#include <Vc/Vc>
-#include <Vc/datapar>
 #ifdef HAVE_CXX_ABI_H
 #include <cxxabi.h>
 #endif
@@ -63,21 +61,7 @@ inline std::string typeToString_impl(std::integral_constant<T, N> const &)
     return s.str();
 }
 // SimdArray to string {{{2
-template <typename T, std::size_t N, typename V, std::size_t M>
-inline std::string typeToString_impl(Vc::SimdArray<T, N, V, M> const &)
-{
-    std::stringstream s;
-    s << "SimdArray<" << typeToString<T>() << ", " << N << '>';
-    return s.str();
-}
-template <typename T, std::size_t N, typename V, std::size_t M>
-inline std::string typeToString_impl(Vc::SimdMaskArray<T, N, V, M> const &)
-{
-    std::stringstream s;
-    s << "SimdMaskArray<" << typeToString<T>() << ", " << N << ", " << typeToString<V>()
-      << '>';
-    return s.str();
-}
+
 // template parameter pack to a comma separated string {{{2
 template <typename T0, typename... Ts>
 std::string typeToString_impl(Typelist<T0, Ts...> const &)
@@ -97,49 +81,9 @@ std::string typeToString_impl(Typelist<T0, Ts...> const &)
     return s.str();
 }
 // Vc::<Impl>::Vector<T> to string {{{2
-template <typename V>
-inline std::string typeToString_impl(
-    V const &, typename std::enable_if<Vc::is_simd_vector<V>::value, int>::type = 0)
-{
-    using T = typename V::EntryType;
-    std::stringstream s;
-    if (std::is_same<V, Vc::Scalar::Vector<T>>::value) {
-        s << "Scalar::";
-    } else if (std::is_same<V, Vc::SSE::Vector<T>>::value) {
-        s << "SSE::";
-    } else if (std::is_same<V, Vc::AVX2::Vector<T>>::value) {
-        s << "AVX::";
-    } else if (std::is_same<V, Vc::MIC::Vector<T>>::value) {
-        s << "MIC::";
-    }
-    s << typeToString<T>() << "_v";
-    return s.str();
-}
-template <typename V>
-inline std::string typeToString_impl(
-    V const &, typename std::enable_if<Vc::is_simd_mask<V>::value, int>::type = 0)
-{
-    using T = typename V::EntryType;
-    std::stringstream s;
-    if (std::is_same<V, Vc::Scalar::Mask<T>>::value) {
-        s << "Scalar::";
-    } else if (std::is_same<V, Vc::SSE::Mask<T>>::value) {
-        s << "SSE::";
-    } else if (std::is_same<V, Vc::AVX2::Mask<T>>::value) {
-        s << "AVX::";
-    } else if (std::is_same<V, Vc::MIC::Mask<T>>::value) {
-        s << "MIC::";
-    }
-    s << typeToString<T>() << "_m";
-    return s.str();
-}
 
 // generic fallback (typeid::name) {{{2
-template <typename T>
-inline std::string typeToString_impl(
-    T const &,
-    typename std::enable_if<!Vc::is_simd_vector<T>::value && !Vc::is_simd_mask<T>::value,
-                            int>::type = 0)
+template <typename T> inline std::string typeToString_impl(T const &)
 {
 #ifdef HAVE_CXX_ABI_H
     char buf[1024];
